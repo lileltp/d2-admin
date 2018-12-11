4722b6be20d1881f66b51b49c0974f822b070023
lileltp <lileltp@163.com>
2018-12-11 09:19:51 +0800

    添加d-form

---
+++ src/components/d2-d-form/components/views/page-panel.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 30, 50, 100]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
+    </el-pagination>
+</template>
+<script>
+export default {
+  props: {
+    total: {
+      type: Number,
+      default: () => {
+        return 0
+      }
+    }
+  },
+  data () {
+    return {
+      currentPage: 1
+    }
+  },
+  methods: {
+    handleSizeChange (size) {
+      this.$emit('size-change', size)
+    },
+    handleCurrentChange (currentPage) {
+      this.currentPage = currentPage
+      this.$emit('current-change', currentPage)
+    }
+  }
+}
+</script>
---
+++ src/components/d2-d-form/components/views/tools-panel.vue
@@ -0,0 +1,107 @@
+<template>
+  <el-button-group>
+    <el-button
+      type="button"
+      icon="fa fa-filter"
+      :size='buttonSize'
+      @click="handleShowFilter"
+      v-if="isShowFilter"
+    >过滤</el-button>
+    <el-button
+      type="button"
+      icon="el-icon-view"
+      :size='buttonSize'
+      @click="handleView"
+      v-if="isShowView"
+    >查看</el-button>
+    <el-button
+      type="button"
+      :size='buttonSize'
+      icon="el-icon-circle-plus-outline"
+      @click="handelAdd"
+      v-if="isShowAdd"
+    >新增</el-button>
+    <el-button
+      type="button"
+      :size='buttonSize'
+      icon="el-icon-edit"
+      @click="handelModify"
+      v-if="isShowUpdate"
+    >修改</el-button>
+    <el-button
+      type="button"
+      :size='buttonSize'
+      icon="el-icon-delete"
+      @click="handelDelete"
+      v-if="isShowDelete"
+    >删除</el-button>
+    <el-button
+      type="button"
+      :size='buttonSize'
+      icon="el-icon-refresh"
+      @click="handelRefresh"
+      v-if="isShowRefresh"
+    >刷新</el-button>
+    <el-button
+      type="button"
+      :size='buttonSize'
+      icon="el-icon-download"
+      @click="handelDownload"
+      v-if="isShowDownload"
+    >下载</el-button>
+    <slot name="extendButton"></slot>
+  </el-button-group>
+</template>
+<script>
+export default {
+  props: {
+    buttonSize: {
+      type: String
+    },
+    isShowAdd: {
+      default: true
+    },
+    isShowView: {
+      default: true
+    },
+    isShowUpdate: {
+      default: true
+    },
+    isShowDelete: {
+      default: true
+    },
+    isShowFilter: {
+      default: true
+    },
+    isShowDownload: {
+      default: false
+    },
+    isShowRefresh: {
+      default: true
+    }
+  },
+  methods: {
+    handleShowFilter () {
+      this.$emit('show-filter')
+    },
+    handleView () {
+      this.$emit('show-view')
+    },
+    handelAdd () {
+      this.$emit('show-add')
+    },
+    handelModify () {
+      this.$emit('show-modify')
+    },
+    handelDelete () {
+      this.$emit('delete')
+    },
+    handelRefresh () {
+      this.$emit('refresh')
+    },
+    handelDownload () {
+      this.$emit('download')
+    }
+  }
+}
+</script>
